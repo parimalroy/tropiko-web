85d6962e71ddb5ef7983ba0f1804f920a882d41b
parimal kumar roy <parimal.tbd@gmail.com>
2022-07-29 12:22:13 +0600

    full width web and slider complete

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  $(".fade").slick({
+    infinite: true,
+    speed: 1500,
+    fade: true,
+    cssEase: "linear",
+    loop: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    prevArrow:
+      '<button type="button" class="slick-prev"><i class="fa-solid fa-chevron-left"></i></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><i class="fa-solid fa-chevron-right"></i></button>',
+  });
+
+  $(".autoplay").slick({
+    loop: true,
+    infinite: true,
+    speed: 1000,
+    autoplay: true,
+    cssEase: "linear",
+    autoplaySpeed: 3000,
+    prevArrow:
+      '<span class="prev-arrow"><i class="fa-solid fa-chevron-left"></i></span>',
+    nextArrow:
+      '<span class="next"><i class="fa-solid fa-chevron-right"></i></span>',
+  });
+});
